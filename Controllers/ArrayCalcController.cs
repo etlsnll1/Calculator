2789a1d6e40778e7fe0a150ddc4f23a35123181a
etlsnll1 <stephen_wigzell@hotmail.com>
2021-10-04 01:46:48 +1100

    Add project files.

---
+++ Controllers/ArrayCalcController.cs
@@ -0,0 +1,56 @@
+﻿using Calculator.InterfaceSpecifications;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Calculator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArrayCalcController : ControllerBase
+    {
+        private readonly ILogger<ArrayCalcController> _logger;
+        private readonly IArrayService _arrayService;
+
+        public ArrayCalcController(IArrayService arrayService, ILogger<ArrayCalcController> logger)
+        {
+            _arrayService = arrayService;
+            _logger = logger;
+        }
+
+        [HttpGet("Reverse")]
+        public ActionResult Reverse([FromQuery] int[] productIds)
+        {
+            try 
+            {
+                var result = _arrayService.Reverse(productIds);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception thrown processing: Reverse");
+                return Error500();
+            }
+        }
+
+        [HttpGet("DeletePart")]
+        public ActionResult DeletePart([FromQuery] int[] productIds, [FromQuery] int position)
+        {
+            try
+            {
+                var result = _arrayService.DeletePart(productIds, position);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception thrown processing: DeletePart");
+                return Error500();
+            }
+        }
+
+        private StatusCodeResult Error500()
+        {
+            return StatusCode(500);
+        }
+    }
+}
